fbf0ef31968283b7e548edd581653cb7c73c5cc1
hopemumbi <mumbimak98@gmail.com>
2023-10-06 11:13:08 +0000

    add 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 /**
+ *main - enty point
  *
- *
- *
+ *Return: always 0
  */
-int main()
+int main(void)
 	{
 		char my_str[] = "\"Programming is like building a multilingual puzzle";
+
 		puts(my_str);
 		return (0);
 	}
