5480814bb441c86f2f9556db31848d8712c782ac
hopemumbi <mumbimak98@gmail.com>
2023-10-09 12:18:02 +0000

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Return: always (0)
+ */
+int main(void)
+{
+	int hex;
+
+	for (hex = 48; hex <= 57; hex++)
+	{
+		putchar(hex);
+	}
+	for (hex =97; hex <= 102; hex++)
+	{
+		putchar(hex);
+	}
+	putchar('\n');
+	return (0);
+}
