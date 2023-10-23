3c8634b732e2d3a6369010455e4b16add5885d1a
hopemumbi <mumbimak98@gmail.com>
2023-10-23 06:43:44 +0000

    4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <string.h>
+/**
+ *print_rev -prints a string, in reverse,
+ *followed by a new line.
+ *@s: pointer to string
+ */
+void print_rev(char *s)
+{
+	int length;
+	int i;
+	length = strlen(s);
+
+	for(i = length - 1; i >= 0; i--)
+	{
+		_putchar(s[i]);
+	}
+	_putchar('\n');
+}
