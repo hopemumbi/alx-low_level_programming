9aee12e6dd7644fa106ab208f031c984f746eea1
hopemumbi <mumbimak98@gmail.com>
2023-10-23 16:21:20 +0000

    100-atoi.c

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * _atoi -  convert a string to an integer.
+ * @s: string
+ * Return: integer
+ */
+int _atoi(char *s)
+{
+	int i, sign, result, digit;
+
+	sign = 1;
+	result = 0;
+	digit = 0;
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] == '-')
+		{
+			sign = -1;
+		}
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			result = result * 10 + s[i] - '0';
+			digit = 1;
+		}
+		else if (digit)
+		{
+			break;
+		}
+	}
+	return (sign * result);
+}
