5e2f6f230977537074b4618c19560be96217ecda
hopemumbi <mumbimak98@gmail.com>
2023-11-07 18:24:06 +0000

    100-rot13.c

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/**
+ *rot13 - t encodes a string into 1337.
+ *@str: string to be encoded
+ *Return: string 1337
+ */
+char *rot13(char *str)
+{
+	int i, j;
+	char chars[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char repl[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	/**loop thru the whole string until the null terminator**/
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		/**loop thru the characters to be replaced**/
+		for (j = 0; chars[j] != '\0'; j++)
+		{
+			/**
+			 * if any character in @str matches any in @chars
+			 * replace it with corresponding @repl
+			 */
+			if (str[i] == chars[j])
+			{
+				str[i] = repl[j];
+				/**exit inner loop once substitution is made**/
+				break;
+			}
+		}
+	}
+	return (str);
+}
+
