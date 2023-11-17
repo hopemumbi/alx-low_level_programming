18e10c3254109a596a380cf0208fb641b1e679b1
hopemumbi <144779094+hopemumbi@users.noreply.github.com>
2023-11-17 11:54:40 +0300

    Update 6-abs.c

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -2,15 +2,13 @@
 /**
  * _abs - computes the absolute value of an integer.
  * @a: random no
- * Return: 'c' or 'a'
+ * Return: '-a' if a < 0 or 'a' if a > 0
  */
 int _abs(int a)
 {
 	if (a < 0)
 	{
-		int c = -c;
-		
-		return (c);
+		return (-a);
 	}
 	else
 	{
