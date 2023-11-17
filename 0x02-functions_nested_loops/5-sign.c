6c0f013dce5264cea7ae2eaedf2e765efc9473cc
hopemumbi <144779094+hopemumbi@users.noreply.github.com>
2023-11-17 11:49:38 +0300

    Update 5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,24 +1,24 @@
 #include "main.h"
 /**
  * print_sign - prints the sign of a number.
- * @n: variable
- * Return:1 or 0 or -1
+ * @n: the number
+ * Return:1  if n > 0 or 0 if n = 0 or -1 if n < 0
  */
 int print_sign(int n)
 {
 	if (n > 0)
 	{
-		_putchar(43);
+		_putchar('+');
 		return (1);
 	}
 	else if (n == 0)
 	{
-		_putchar(48);
+		_putchar('0');
 		return (0);
 	}
 	else
 	{
-		_putchar(45);
+		_putchar('-');
 		return (-1);
 	}
 }
