7a91eb5d2088b27966adecb85279e14306934147
hopemumbi <mumbimak98@gmail.com>
2023-11-20 06:26:45 +0000

    update 102-fibonacci.c

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -6,20 +6,19 @@
  */
 int main(void)
 {
-	int n = 50;
 	int i;
-	long int fib[n];
+	long int fib[50];
 
 	fib[0] = 1;
 	fib[1] = 2;
-	for (i = 2; i < n; i++)
+	for (i = 2; i < 50; i++)
 	{
 		fib[i] = fib[i - 1] + fib[i - 2];
 	}
-	for (i = 0; i < n - 1; i++)
+	for (i = 0; i < 49; i++)
 	{
 		printf("%lu, ", fib[i]);
 	}
-	printf("%lu\n", fib[n - 1]);
+	printf("%lu\n", fib[49]);
 	return (0);
 }
