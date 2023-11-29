a108e3930e7fd1b2133a955c5fbd53f1f5cedb66
hopemumbi <mumbimak98@gmail.com>
2023-11-29 17:07:44 +0000

    function pointers

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,9 @@
+/**
+ *print_name - calls a function pointed to by f with the provided name
+ *@name: the name to be printed
+ * @f: a function pointer to a function - takes a char pointer as an argument
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
