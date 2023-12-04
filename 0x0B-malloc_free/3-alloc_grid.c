57fc095e22a84d0106b91c55bfea3b6a95f80272
hopemumbi <mumbimak98@gmail.com>
2023-12-04 05:37:32 +0000

    update 3-alloc_grid.c

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -12,16 +12,16 @@ int **alloc_grid(int width, int height)
 
 	if (width <= 0 || height <= 0)
 		return (NULL);
-
-	/*Allocate memory for rows*/
-	ar = malloc(height * sizeof(int));
-	if (ar == NULL)
+	/*Allocate memory for rows(array  of pointers)*/
+	ar = (int **)malloc(height * sizeof(int *));
+	if (ar == NULL)/*check if memory allocation fails*/
 		return (NULL);
 
 	/*Allocate memory for columns in each row*/
 	for (i = 0; i < height; i++)
 	{
-		ar[i] = malloc(width * sizeof(int));
+		ar[i] = (int *)malloc(width * sizeof(int));
+		/*check if memory allocation was successful*/
 		if (ar[i] == NULL)
 		{
 			/*If allocation fails, free memory and return NULL*/
@@ -32,6 +32,12 @@ int **alloc_grid(int width, int height)
 		}
 	}
 
+	/*Each element of the grid should be initialized to 0*/
+	for (i = 0; i < height; i++)
+		for (j = 0; j < width; j++)
+		{
+			ar[i][j] = 0;
+		}
 
 	return (ar);
 }
