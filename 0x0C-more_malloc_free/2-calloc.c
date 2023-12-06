b74865a835e51d3e8de0adc91c97905bc6df6d59
hopemumbi <mumbimak98@gmail.com>
2023-12-06 06:19:05 +0000

    update 2-calloc.c

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,6 +1,28 @@
 #include <stdlib.h>
 #include <string.h>
 /**
+ *_memset - fills memory with a constant byte.
+ *@s:pointer to memory
+ *@b:a constant byte
+ *@n:number of bytes to fill
+ *Return: 's'
+ */
+void *_memset(char *s, char b, unsigned int n)
+{
+	/*pointer to the memory area 's'*/
+	char *ptr = s;
+
+	/*counter : tracks the no of bytes filled*/
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+		/*assign value b to location pointed to by 's' at index i*/
+		s[i] = b;
+
+	return (ptr);
+}
+
+/**
  *_calloc -  allocates memory for an array, using malloc.
  *@nmemb: no of elements in an array
  *@size: size of each element in bytes
@@ -11,20 +33,20 @@
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	int *ptr;
+	void *pptr;
 
 	/*Check if nmemb or size is 0*/
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 
 	/*Allocate memory for nmemb * size elements and set to zero*/
-	ptr =  malloc(nmemb * size);
+	pptr =  malloc(nmemb * size);
 
 	/*Check if malloc fails*/
-	if (ptr == NULL)
+	if (pptr == NULL)
 		return (NULL);
 
-	memset(ptr, 0, nmemb * size);
+	_memset(pptr, 0, nmemb * size);
 	/*Memory is already set to zero due to calloc behavior*/
-	return (ptr);
+	return (pptr);
 }
