3d74762e0fec0538154a96e560c4dc9fdcb4eb75
hopemumbi <144779094+hopemumbi@users.noreply.github.com>
2023-12-06 11:43:03 +0300

    Update 100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -11,17 +11,10 @@ int _atoi(char *s)
 	int result = 0;
 	int sign = 1;
 
-	while (s[i] == ' ')
-		i++;
-
-	if (s[i] == '-')
+	while (s[i] == ' ' || (s[i] == '-' || s[i] == '+'))
 	{
-		sign = -1;
-		i++;
-	}
-	else if (s[i] == '+')
-	{
-		sign = 1;
+		if (s[i] == '-')
+			sign *= -1;
 		i++;
 	}
 	while (s[i] >= '0' && s[i] <= '9')
