c87bea80153f53fe7a6bde837b60e1442236f5a4
hopemumbi <mumbimak98@gmail.com>
2023-12-06 08:59:45 +0000

    update 100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -11,30 +11,35 @@ int _atoi(char *s)
 	int result = 0;
 	int sign = 1;
 
+	/*skip spaces and handle symbols '-' and '+'*/
 	while (s[i] == ' ' || (s[i] == '-' || s[i] == '+'))
 	{
 		if (s[i] == '-')
 			sign *= -1;
 		i++;
 	}
+
+	/*skip non-numeric/alpha characters*/
 	while (s[i] < '0' || s[i] > '9')
 	{
 		if (s[i] == '\0')
-			return (0);
+			return (0);/*when no valid no is found*/
 		i++;
 	}
+
+	/*Parse the numeric part of the string*/
 	while (s[i] >= '0' && s[i] <= '9')
+	{
+		if (result > INT_MAX / 10
+			|| (result == INT_MAX / 10
+			&& s[i] - '0' > 7))
 		{
-			if (result > INT_MAX / 10
-				|| (result == INT_MAX / 10
-				&& s[i] - '0' > 7))
-			{
-				if (sign == 1)
-					return (INT_MAX);
-				else
-					return (INT_MIN);
-			}
-			result = result * 10 + (s[i++] - '0');
+			if (sign == 1)
+				return (INT_MAX);
+			else
+				return (INT_MIN);
 		}
+		result = result * 10 + (s[i++] - '0');
+	}
 	return (sign * result);
 }
