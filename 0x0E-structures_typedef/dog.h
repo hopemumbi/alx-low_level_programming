81dd4f24b88295a298f43baf1b12afa8e0d0c1c6
hopemumbi <mumbimak98@gmail.com>
2023-12-14 08:07:33 +0000

    add dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,13 @@
+/**
+ *struct dog - define a new structure
+ *@name:first member of type pointer to char
+ *@age: second member of type float
+ *@owner: third member of type pointer to char
+ *
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
