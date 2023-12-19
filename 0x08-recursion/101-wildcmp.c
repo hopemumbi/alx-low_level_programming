2ee2b64abb4ae1706494772ffe3d54a0126aa5f8
hopemumbi <mumbimak98@gmail.com>
2023-12-19 07:55:37 +0000

    update 101-wildcmp.c

---
+++ 0x08-recursion/101-wildcmp.c
@@ -0,0 +1,24 @@
+/**
+ *wildcmp - compares two strings
+ *@s1:the first string
+ *@s2:the second string with wildcards(s)
+ *
+ *Return:1 if the strings can be considered identical, otherwise return 0
+ */
+int wildcmp(char *s1, char *s2)
+{
+	/* If both strings are empty, they are considered identical */
+	if (*s1 == '\0' && *s2 == '\0')
+		return (1);
+
+	 /* If the current characters match or s2 has a wildcard (*) */
+	if (*s1 == *s2 || *s2 == '*')
+	{
+		/* Try to match the rest of the strings */
+		if (wildcmp(s1 + 1, s2 + 1) || wildcmp(s1, s2 + 1) || wildcmp(s1 + 1, s2))
+			return (1);
+	}
+
+	/* If no match is found, return 0 */
+	return (0);
+}
