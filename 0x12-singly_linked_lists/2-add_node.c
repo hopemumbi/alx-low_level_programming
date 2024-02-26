d696d1945513e3f6da64c6c6043c73179a79d897
hopemumbi <mumbimak98@gmail.com>
2024-02-26 08:44:54 +0000

    update 2-add_node.c

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -12,19 +12,29 @@
  */
 list_t *add_node(list_t **head, const char *str)
 {
-	list_t *new;
+	list_t *new;/*Declare a pointer to a new list node*/
 
+	/**
+	 * Allocate memory for the new node
+	 *  Check if memory allocation was successful
+	 *  Return NULL if memory allocation failed
+	 */
 	new = malloc(sizeof(list_t));
 	if (new == NULL)
 		return (NULL);
 
+	/*str = Duplicate the input string*/
 	new->str = strdup(str);
 
+	/*len = length of the input string*/
 	new->len = strlen(str);
 
+	/*next = points to the current head of the list*/
 	new->next = *head;
 
+	/*Update the head pointer to point to the new node, making it the new head*/
 	*head = new;
 
+	/*Return the address of the new node*/
 	return (new);
 }
