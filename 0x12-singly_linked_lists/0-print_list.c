c98595b2bf999ccdec51205eebdf3c3825340e7b
hopemumbi <mumbimak98@gmail.com>
2024-02-26 07:46:47 +0000

    update 0-print_list.c

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "lists.h"
+/**
+ *print_list - Prints all the elements of a linked list
+ *@h: Pointer to the head of the list
+ *
+ *
+ *Return: the number of nodes
+ */
+size_t print_list(const list_t *h)
+{
+	size_t i = 0;
+
+	while (h != NULL)
+	{
+		printf("[%u] %s\n", h->len, h->str);
+		h = h->next;
+		i++;
+	}
+	return (i);
+}
