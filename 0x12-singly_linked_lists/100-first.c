33cdd1459eafc1518320ec1c8e648adcf305ab26
hopemumbi <mumbimak98@gmail.com>
2024-02-26 13:50:35 +0000

    update 100-first.c

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * before_main -  prints
+ * You're beat! and yet, you must allow,\nI bore my house upon my back!\n
+ * before the main function is executed.
+ *
+ * Return: always 0
+ */
+void __attribute__((constructor)) before_main();
+
+void before_main(void)
+{
+	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+}
+
