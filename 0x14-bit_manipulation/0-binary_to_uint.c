a3131b7c3bdc649a4305a53b14bd4b49447b501d
hopemumbi <mumbimak98@gmail.com>
2024-02-28 14:48:08 +0000

    update 0-binary_to_uint.c

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -12,12 +12,16 @@ unsigned int binary_to_uint(const char *b)
 	int i, uint = 0, len = strlen(b), power = 1;
 
 	if (b == NULL)
+	{
 		return (0);
+	}
 
 	for (i = len - 1; i >= 0; i--)
 	{
 		if (b[i] != '0' && b[i] != '1')
+		{
 			return (0);
+		}
 
 		if (b[i] == '1')
 		{
