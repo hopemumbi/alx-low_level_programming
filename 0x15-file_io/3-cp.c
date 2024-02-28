b81f493ea1c0d0a2bf5a76f5e6aa664071ce84c6
hopemumbi <mumbimak98@gmail.com>
2024-02-28 09:24:15 +0000

    update 3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -25,7 +25,7 @@ int main(int ac, char **av)
 	if (o_from == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from %s\n", av[1]);
-		exit(99);
+		exit(98);
 	}
 
 	o_to = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
@@ -33,6 +33,7 @@ int main(int ac, char **av)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
 		close(o_from);
+		exit(99);
 	}
 
 	while ((readd = read(o_from, buffer, BUF_SIZE)) > 0)
@@ -41,6 +42,8 @@ int main(int ac, char **av)
 		if ((written == -1) || (written != readd))
 		{
 			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
+			close(o_from);
+			close(o_to);
 			exit(99);
 		}
 		
