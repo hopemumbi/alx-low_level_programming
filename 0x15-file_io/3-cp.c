14b1d9470193f582e0062022b5b7e89d98605ac1
hopemumbi <mumbimak98@gmail.com>
2024-02-28 09:33:53 +0000

    update 3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -18,41 +18,38 @@ int main(int ac, char **av)
 
 	if (ac != 3)
 	{
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-		exit(97);
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
 	}
 	o_from = open(av[1], O_RDONLY);
 	if (o_from == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from %s\n", av[1]);
-		exit(98);
+		dprintf(STDERR_FILENO, "Error: Can't read from %s\n", av[1]), exit(98);
 	}
-
-	o_to = open(av[2], O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
+	o_to = open(av[2], O_WRONLY | O_CREAT | O_TRUNC,
+			S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
 	if (o_to == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
-		close(o_from);
-		exit(99);
+		close(o_from), exit(99);
 	}
-
 	while ((readd = read(o_from, buffer, BUF_SIZE)) > 0)
 	{
 		written = write(o_to, buffer, readd);
 		if ((written == -1) || (written != readd))
 		{
 			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]);
-			close(o_from);
-			close(o_to);
-			exit(99);
+			close(o_from), close(o_to), exit(99);
 		}
-		
+	}
+	if (readd == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from %s\n", av[1]);
+		exit(98);
 	}
 
 	if ((close(o_from) == -1) || (close(o_to) == -1))
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd\n");
-		exit(100);
+		dprintf(STDERR_FILENO, "Error: Can't close fd\n"), exit(100);
 	}
 	return (0);
 }
