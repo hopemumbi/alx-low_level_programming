59c4cfa7a72c2443b31d8fa49600dd18437701db
hopemumbi <mumbimak98@gmail.com>
2024-02-29 06:26:44 +0000

    update 2-get_bit.c

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -11,13 +11,14 @@ int get_bit(unsigned long int n, unsigned int index)
 {
 	unsigned long int mask;
 
+	/* check if index is within range */
+	if (index >= sizeof(unsigned long int) * 8)
+		return (-1);
 	mask = 1UL << index;
 
-	/*Check if the current bit is set */
+	/*Check if the bit at index is set */
 	if (n & mask)
 		return (1);
 	else
 		return (0);
-
-	return (-1);
 }
