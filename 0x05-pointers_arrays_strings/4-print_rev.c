e1f7041b0d1333af3ee3769dff687b24f401e26e
hopemumbi <mumbimak98@gmail.com>
2024-04-18 11:29:47 +0300

    update 4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,6 +1,28 @@
 #include "main.h"
 #include <stdio.h>
 /**
+ * _strlen - calculates the length of a string.
+ *
+ * @s: a pointer to the string.
+ *
+ * Return: the length of the string.
+ */
+int _strlen(char *s)
+{
+	/* Initialize a variable to store the length of the string */
+	int count = 0;
+	/* Iterate through the string until reaching the null terminator */
+	while (*s != '\0')
+	{
+		/* Increment the length for each character in the string */
+		count += 1;
+		s++;
+	}
+	/* Return the length of the string */
+	return (count);
+}
+
+/**
  * print_rev - print a string in reverse
  *
  * @s: a pointer to a string
