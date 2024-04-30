761c517264d3dc6cda1e68ce18d01ba61bd4b8ea
hopemumbi <mumbimak98@gmail.com>
2024-04-30 14:22:27 +0300

    update 3-print_all.c

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,46 +1,95 @@
+#include "variadic_functions.h"
 #include <stdarg.h>
 #include <stdio.h>
 /**
- *print_all - that prints anything.
- *@format:  list of types of arguments passed to the function
- *...: other variables
+ * print_char - prints a char
+ *
+ * @args: A list of arguments; format specifiers
+ */
+void print_char(va_list args)
+{
+	printf("%c", va_arg(args, int));
+}
+
+/**
+ * print_int - prints an integer
+ *
+ * @args: A list of arguments; format specifiers
+ */
+void print_int(va_list args)
+{
+	printf("%d", va_arg(args, int));
+}
+
+/**
+ * print_float - prints a float number
+ *
+ * @args: A list of arguments; format specifiers
+ */
+void print_float(va_list args)
+{
+	printf("%f", va_arg(args, double));
+}
+
+/**
+ * print_str - prints a string
+ *
+ * @args: A list of arguments; format specifiers
+ */
+void print_str(va_list args)
+{
+	char *s;
+
+	s = va_arg(args, char *);
+	if (s == NULL)
+		printf("(nil)");
+	else
+		printf("%s", s);
+}
+
+/**
+ * print_all - that prints anything.
+ *
+ * @format:  list of types of arguments passed to the function
+ * ...: other variables
  */
 void print_all(const char * const format, ...)
 {
 	va_list args;
-	int i = 0;
-	char *s;
+	int i = 0, j = 0;
+	const char *separator = "";
 
+	/* Array of structs to map format specifiers to print functions*/
+	print_f flist[] = {
+		{"c", print_char},
+		{"i", print_int},
+		{"f", print_float},
+		{"s", print_str},
+	};
+
+	/* Start processing variable arguments */
 	va_start(args, format);
 
+	/* Loop through each character in the format string */
 	while (format[i] != '\0')
-		/*prints a comma and a space to separate the items being printed.*/
-		if (i > 0)
-			printf(", ");
+	{
+		j = 0; /* Reset inner loop counter */
 
-		switch (format[i])
+		/* Check for matching specifier in the flist array*/
+		while (j < 4 && (format[i] != *(flist[j].specifier)))
+			j++;
+
+		/* If a matching specifier is found */
+		if (j < 4)
 		{
-			case 'c':
-				printf("%c", va_arg(args, int));
-				break;
-			case 'i':
-				printf("%d", va_arg(args, int));
-				break;
-			case 'f':
-				printf("%f", va_arg(args, double));
-				break;
-			case 's':
-				s = va_arg(args, char *);
-				if (s == NULL)
-				{
-					printf("(null)");
-				}
-				printf("%s", s);
-				break;
-			default:
-				break;
-
+			/*Print separator except for the first element*/
+			printf("%s", separator);
+			/*Call the corresponding print function from flist based on the specifier*/
+			flist[j].print(args);
+			/*Update separator for subsequent elements*/
+			separator = ", ";
 		}
+		/* Move to the next character in the format string */
 		i++;
 	}
 	va_end(args);
