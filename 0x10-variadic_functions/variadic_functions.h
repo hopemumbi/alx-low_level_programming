d03460d219ca952d45372a2c1b9be96af42a81ea
hopemumbi <mumbimak98@gmail.com>
2024-04-30 14:23:23 +0300

    update variadic_functions.h

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,5 +1,18 @@
 #ifndef MAIN_H
 #define MAIN_H
+#include <stdarg.h>
+/**
+ * print_f - Defines a function that prints 
+ *
+ * @specifier: A character that represents a data type
+ * @print: A function pointer to a function that prints a data type
+ */
+typedef struct print_f
+{
+	char *specifier;
+	void (*print)(va_list args);
+} print_f;
+
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
