e0c2d9aeb065dd05ce02f6b95becab8324c13348
hopemumbi <mumbimak98@gmail.com>
2024-05-08 11:56:35 +0300

    update 104-fibonacci.c

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * main - prints the first 50 Fibonacci numbers, starting with 1 and 2,
+ * main - prints the first 98 Fibonacci numbers, starting with 1 and 2,
  * followed by a new line.
  * Return: 0 if successful
  */
@@ -16,7 +16,7 @@ int main(void)
 	/*Print the first two Fibonacci numbers*/
 	printf("%lu, %lu", a, b);
 	/*Generate and print the next 96 Fibonacci numbers*/
-	for (i = 2; i < 98; i++)
+	for (i = 2; i < 96; i++)
 	{
 		/*Calculate the next Fibonacci number in the sequence*/
 		next = a + b;
